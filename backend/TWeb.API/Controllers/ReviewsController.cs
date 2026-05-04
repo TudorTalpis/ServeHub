c9f9c3afb455985b5866844a68e59b41a3c054be
Noroc Ion <nicu.norok@gmail.com>
2026-05-04 21:39:07 +0300

    Refactored Reviews controller

--- backend/TWeb.API/Controllers/ReviewsController.cs
+++ backend/TWeb.API/Controllers/ReviewsController.cs
@@ -5,27 +5,51 @@ using TWeb.BusinessLayer.Interfaces;
 namespace TWeb.API.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/v1/[controller]")]
 public class ReviewsController : ControllerBase
 {
     private readonly IReviewService _reviewService;
+    private readonly ILogger<ReviewsController> _logger;
 
-    public ReviewsController(IReviewService reviewService)
+    public ReviewsController(
+        IReviewService reviewService,
+        ILogger<ReviewsController> logger)
     {
         _reviewService = reviewService;
+        _logger = logger;
     }
 
     [HttpGet]
-    public IActionResult GetAll() => Ok(_reviewService.GetAll());
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<ReviewDto>>> GetAll(CancellationToken ct)
+    {
+        var reviews =  _reviewService.GetAll();
+        return Ok(reviews);
+    }
+    
 
     [HttpGet("provider/{providerId}")]
-    public IActionResult GetByProviderId(string providerId) =>
-        Ok(_reviewService.GetByProviderId(providerId));
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<ReviewDto>>> GetByProviderId(
+        string providerId,
+        CancellationToken ct)
+    {
+        var reviews = _reviewService.GetByProviderId(providerId);
+        return Ok(reviews);
+    }
 
     [HttpPost]
-    public IActionResult Create([FromBody] CreateReviewDto dto)
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<ActionResult<ReviewDto>> Create(
+        [FromBody] CreateReviewDto dto,
+        CancellationToken ct)
     {
-        var r = _reviewService.Create(dto);
-        return CreatedAtAction(nameof(GetAll), r);
+        var createdReview =  _reviewService.Create(dto);
+
+        return CreatedAtAction(
+            nameof(GetAll),
+            new { id = createdReview.Id },
+            createdReview);
     }
 }
+
